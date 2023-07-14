342c4e2adfa00a7b6f9b6ea52f00b421e6012902
lucasmattos <lucassousa.d.m251@gmail.com>
2023-07-14 16:25:41 -0300

    refactor: grouping routes, refactoring components

--- src/roteador/index.ts
+++ src/roteador/index.ts
@@ -1,33 +1,36 @@
-import { createRouter,RouteRecordRaw, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import Tarefas from '../views/Tarefas.vue'
 import Projetos from '../views/Projetos.vue'
 import Formulario from '../views/Projetos/Formulario.vue'
+import Lista from '../views/Projetos/Lista.vue'
 
 const rotas: RouteRecordRaw[] = [
-    
     {
         path: '/',
-        name: 'tarefas',
+        name: 'Tarefas',
         component: Tarefas
     },
-
     {
-        path: '/Projetos',
-        name: 'projetos',
-        component: Projetos
-    },
-
-    {
-        path: '/Projetos/novo',
-        name: ' Novo projeto',
-        component: Formulario
-    },
-
-    {
-        path: '/Projetos/:id',
-        name: ' Editar projeto',
-        component: Formulario,
-        props: true
+        path: '/projetos',
+        component: Projetos,
+        children: [
+            {
+                path: '',
+                name: 'Projetos',
+                component: Lista
+            },
+            {
+                path: 'novo',
+                name: 'Novo projeto',
+                component: Formulario
+            },
+            {
+                path: ':id',
+                name: 'Editar projeto',
+                component: Formulario,
+                props: true
+            },
+        ]
     }
 ]
 
--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -1,52 +1,16 @@
 <template>
   <section class="projetos">
     <h1 class="title">Projetos</h1>
-
-    <router-link to="/projetos/novo" class="button">
-      <span class="icon is-small">
-        <i class="fas fa-plus"></i>
-      </span>
-      <span>Novo projeto</span>
-    </router-link>
-
-    <table class="table is-fullwidth">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Nome</th>
-          <th>Ações</th>
-        </tr>
-      </thead>
-
-      <tbody>
-        <tr v-for="projeto in projetos" :key="projeto.id">
-          <td>{{ projeto.id }}</td>
-          <td>{{ projeto.nome }}</td>
-          <td>
-            <router-link :to="`/projetos/${projeto.id}`" class="button">
-              <span class="icon is-small">
-                <i class="fas fa-pencil-alt"></i>
-              </span>
-            </router-link>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <Router-view></Router-view>
   </section>
 </template>
 
 <script lang="ts">
-import { useStore } from "@/store";
-import { computed, defineComponent } from "vue";
+
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: "Projetos",
-  setup() {
-    const store = useStore();
-    return {
-      projetos: computed(() => store.state.projetos),
-    };
-  },
+  name: "projetos",
 });
 </script>
 
@@ -55,3 +19,4 @@ export default defineComponent({
   padding: 1.25rem;
 }
 </style>
+
